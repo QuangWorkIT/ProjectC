a5184d13a24b83bf7390adc019d20af1ed414877
Quang <nguyenminhquangmmqlamdong@gmail.com>
2024-04-21 01:03:11 +0700

    test commit

--- src/Ticket/main.c
+++ src/Ticket/main.c
@@ -15,7 +15,6 @@ int main(){
         switch (choice)
         {
             case 1:
-                printf("hello world");
                 printf("Create a new new tickets\n");
                 // hàm tạo vé trong ticket_create.c
                 create_ticket(tickets, &current_id);
